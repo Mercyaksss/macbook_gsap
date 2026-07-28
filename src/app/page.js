6c2915ec4da3a96356246c21dc7fb9e8b1f7814c
Mercyaksss <mercyaksss625@gmail.com>
2026-07-28 23:09:13 +0100

    Implement 3D product viewer

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
-import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import ProductViewer from "./components/ProductViewer";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   return (
    <main>
     <Navbar/>
     <Hero/>
+    <ProductViewer/>
    </main>
   );
 }
