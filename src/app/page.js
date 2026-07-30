cb4923a016a3918bb0cff22ec4571fdc395c678b
Mercyaksss <mercyaksss625@gmail.com>
2026-07-30 13:18:39 +0100

    Implement performance section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import ProductViewer from "./components/ProductViewer";
 import Showcase from "./components/Showcase";
+import Performance from "./components/Performance";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from 'gsap/all';
 gsap.registerPlugin(ScrollTrigger);
@@ -13,6 +14,7 @@ export default function Home() {
     <Hero/>
     <ProductViewer/>
     <Showcase/>
+    <Performance/>
    </main>
   );
 }
